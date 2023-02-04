43e9a0a7293d0089c2c4e5ef77abecc18a2d1b44
Anshika Bhasin <41538257+anshika109@users.noreply.github.com>
2023-02-04 12:02:30 +0530

    Update script.sh

--- terrafom/script.sh
+++ terrafom/script.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# sudo su
-# yum update -y
-# yum install httpd -y
-# touch /var/www/html/index.html
-# echo "Hello" > /var/www/html/index.html
-# systemctl start httpd.service
-# systemctl enable httpd.service
 sudo su
 yum update -y
 amazon-linux-extras install java-openjdk11
@@ -18,5 +11,3 @@ cd bin
 chmod +x /opt/apache-tomcat-9.0.63/bin/*.sh
 cp /var/lib/jenkins/workspace/Terraform/target/LoginWebApp-1.war /opt/apache-tomcat-9.0.63/webapps/
 ./catalina.sh start
-# /opt/tomcat/apache-tomcat-9.0.70/webapps/
-# sh /opt/tomcat/apache-tomcat-9.0.70/bin/catalina.sh stop && nohup /opt/tomcat/apache-tomcat-9.0.70/bin/catalina.sh start
