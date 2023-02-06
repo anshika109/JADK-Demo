3df625c6e88ebd903e59b3ea89e982555c84a5c5
Anshika Bhasin <41538257+anshika109@users.noreply.github.com>
2023-02-06 10:43:05 +0530

    Update script.sh

--- terrafom/script.sh
+++ terrafom/script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 sudo su
 yum update -y
+cd /opt
+wget --user=admin --password=admin123 http://13.232.252.171:8081/repository/demo/com/ltidevops/LoginWebApp/1/LoginWebApp-1.war
 amazon-linux-extras install java-openjdk11
 yum -y install wget
-cd /opt
 wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.63/bin/apache-tomcat-9.0.63.tar.gz
 tar -zvxf apache-tomcat-9.0.63.tar.gz
 cd apache-tomcat-9.0.63
 cd bin
 chmod +x /opt/apache-tomcat-9.0.63/bin/*.sh
-cp /var/lib/jenkins/workspace/Terraform/target/LoginWebApp-1.war /opt/apache-tomcat-9.0.63/webapps/
-./catalina.sh start
+cd ~
+mv /opt/LoginWebApp-1.war /opt/apache-tomcat-9.0.63/webapps/
+sh /opt/apache-tomcat-9.0.63/bin/catalina.sh stop && nohup /opt/apache-tomcat-9.0.63/bin/catalina.sh start
